d3f214cfb81dc3c0470408c199ddf23c9e9de4be
aman kumar <ak9031821202@gmail.com>
2022-08-09 15:50:20 +0530

    add dummyCom file

--- src/component/ProgressDetailsBar.js
+++ src/component/ProgressDetailsBar.js
@@ -1,25 +1,27 @@
-// @flow 
+
 import {  Grid, Typography } from '@mui/material';
 import React from 'react';
 import ProgressBar from './ProgressBar';
-import PhotoIcon from '@mui/icons-material/Photo';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const ProgressDetailsBox = () => {
+
+    
     return (
-        <Grid class='flex flex-row justify-between w-full'>
-            <Grid class='inline'>
-               <PhotoIcon sx={{ fontSize: "12px",color:'#233554' }}/>
+        <Grid class='flex flex-row justify-between items-start w-full'>
+            <Grid class='inline mt-1'>
+               <InsertPhotoOutlinedIcon sx={{ fontSize: "24px",color:'#233554',backgroundColor:'#C5D2E7',padding:'4px',borderRadius:'14px' }}/>
             </Grid>
             <Grid class='w-full'>
-                <Grid class='ml-3'>
-                    <Typography class='text-sm font-sm font-Inter'>Page Cover.jpg</Typography>
-                    <Typography class='font-normal text-xs text-gray-500 font-Inter'>254mb</Typography>
+                <Grid class='ml-3 pt-2'>
+                    <Typography class='text-sm font-sm font-Inter'>image</Typography>
+                    <Typography class='font-normal text-xs text-gray-500 font-Inter'>500 KB</Typography>
                     <ProgressBar/>
                 </Grid>
                 </Grid>
               <Grid>
-                  <CheckCircleIcon sx={{ fontSize: "12px",color:'#233554' }}/>
+                  <CheckCircleIcon  sx={{ fontSize: "14px",color:'#233554' }}/>
               </Grid>
         </Grid>
     );
